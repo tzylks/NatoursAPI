70feb63779621f9c511065c4ca0ae9b47c543612
TrevorZylks <trevorzylks@gmail.com>
2021-12-09 17:25:17 -0700

    readme

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ npm run start:dev
 ```
 
 ## Visit localhost:8000/api/v1/tours
+
+*Endpoints of interest
+
+- api/v1/tours/tour-stats
+- api/v1/tours/top-5-cheap
+
+# Querying
+
+**pagination example**
+- api/v1/tours?page=1&limit=10
+
+**sort example**
+- api/v1/tours?sort=price
+
+**filter examples**
+
+- the following can be passed after keywords: [gte, lte, ge, le]
+
+- api/v1/tours&price[gte]=500 // for great than or equal to
+- api/v1/tours&price[lte]=500 // for less than or equal to
+- api/v1/tours&price[gt]=500 // for greater than 
+api/v1/tours&price[lt]=500 // for less than
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import slugify from 'slugify'
 
 //CREATE THE SHAPE OF OUR DATA, INCLUDING VALIDATORS
 const tourSchema = new mongoose.Schema({
@@ -47,21 +48,56 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have an image']
     },
     images: [String],
+    slug: {
+        type: String
+    },
     createdAt: {
         type: Date,
         default: Date.now,
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    },
 }, {
     toJSON: { virtuals: true},
     toObject: { virtuals: true}
 })
 
+//FOR ITEMS YOU DO NOT WANT TO COMMIT TO DB
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7
 })
 
+//DOCUMENT MIDDLEWARE
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true })
+    next();
+})
+
+// tourSchema.pre('save', function(next) {
+//     console.log('Will save document...')
+//     next();
+// })
+
+// tourSchema.post('save', function(doc, next) {
+//     console.log(doc)
+//     next()
+// })
+
+//QUERY MIDDLEWARE (FOR ALL STRINGS THAT START WITH FIND SUCH AS FINDONE FINDONEANDDELETE ETC.)
+tourSchema.pre(/^find/, function(next) {
+    this.find({ secretTour: { $ne: true } })
+    next()
+})
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next){
+    this.pipeline().unshift(({$match: {secretTour: {$ne: true}}}))
+    next()
+})
 //CAPITL LETTERS FOR MODEL
 const Tour = mongoose.model('Tour', tourSchema)
 
